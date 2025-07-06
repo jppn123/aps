57d33a749737a5b19f8fb80742505eae6a3a4bad
João Pedro Barros de Moura <59260587+jppn123@users.noreply.github.com>
2025-07-06 20:12:40 -0300

    update base archives

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from connection import create_db_and_tables
 from routers import *
+from routers.face import router as face_router
 
 
 def lifespan(app:FastAPI):
@@ -11,14 +12,18 @@ def lifespan(app:FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
-    CORSMiddleware, 
-    allow_origins="*", 
-    allow_methods="*",
-    allow_headers="*"
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 app.include_router(horas.router)
 app.include_router(usuario.router)
 app.include_router(login.router)
 app.include_router(utils.router)
+app.include_router(ponto.router)
+app.include_router(face_router)
 
--- readme.md
+++ readme.md
@@ -12,3 +12,16 @@ pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trus
 # gerar senha do email
 
 https://myaccount.google.com/u/3/apppasswords
+
+# rodar para poder funcionar no celular fisico
+
+```bash
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+```
+
+precisou instalar o cmake vindo de cmake.org
+
+pacote da dlib para rodar a biblioteca face_recognition
+```bash
+pip install https://github.com/RvTechiNNovate/dlib/releases/download/19.24.2/dlib-19.24.2-cp311-cp311-win_amd64.whl
+```
